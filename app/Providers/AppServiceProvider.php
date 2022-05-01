e5e53b2e4b497dd50264d11db61583e69f4963e3
Александр Шеин <shein.a@gigwork.ru>
2022-05-01 22:07:32 +0700

    зарегистрирован redis Client в сервис провайдере

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Predis\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Client::class, function (): Client {
+            $config = config('database.redis.default');
+            return new Client($config);
+        });
     }
 
     /**
